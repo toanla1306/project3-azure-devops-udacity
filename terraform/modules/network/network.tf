b720d5ad697c6fce25c3a8bb590db9c419042e54
toanla1306 <ldtoan1306@gmail.com>
2024-10-02 12:44:47 +0700

    update name vpc

--- terraform/modules/network/network.tf
+++ terraform/modules/network/network.tf
@@ -5,7 +5,7 @@ resource "azurerm_virtual_network" "test" {
   resource_group_name  = "${var.resource_group}"
 }
 resource "azurerm_subnet" "test" {
-  name                 = "${var.application_type}-${var.resource_type}-sub"
+  name                 = "${var.application_type}-${var.resource_type}"
   resource_group_name  = "${var.resource_group}"
   virtual_network_name = "${azurerm_virtual_network.test.name}"
   address_prefixes       = ["${var.address_prefix_test}"]
